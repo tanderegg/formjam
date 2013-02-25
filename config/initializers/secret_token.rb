c2eaca377a673af94fd209bea893fd7495ce36fe
Tim Anderegg <tim@neworganizing.com>
2013-02-24 20:47:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Formjam::Application.config.secret_key_base = 'e893a614f2ec45798b9bea8cf0ca5b3023a8edc95d860d440c9457047d17a754899f1b048285e353d1d896a9fee3363100c622b6a6faf0abee308af9878b417b'
